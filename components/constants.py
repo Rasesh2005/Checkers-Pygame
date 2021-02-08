a267b905ff3a7433b20c9dbc50163cc63eeacb3e
Rasesh Shetty <rasesh.udayshetty@gmail.com>
2021-02-08 20:09:00 +0530

    Initial Commit

---
+++ components/constants.py
@@ -0,0 +1,14 @@
+import pygame
+
+WIDTH = HEIGHT = 640
+SQUARE_SIZE = WIDTH//8
+
+# Colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+BLUE = (0, 0, 255)
+RED = (255, 0, 0)
+GREY = (128, 128, 128)
+
+# King peice Crown Image
+CROWN=pygame.transform.scale(pygame.image.load('assets/crown.png',(40,20)))
